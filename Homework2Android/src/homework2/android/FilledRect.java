d58706bb920213e726dced8ce1b66143a82bdf94
julenka <julenka@cs.cmu.edu>
2013-02-05 02:51:11 -0500

    Adding GraphicalObjectBase class

--- Homework2Android/src/homework2/android/FilledRect.java
+++ Homework2Android/src/homework2/android/FilledRect.java
@@ -8,14 +8,10 @@ import android.graphics.Paint.Style;
 import android.graphics.Path;
 import android.graphics.Rect;
 
-public class FilledRect implements GraphicalObject {
+public class FilledRect extends GraphicalObjectBase {
 
 	protected Paint m_paint = new Paint();
 	protected Rect m_rect = new Rect();
-	protected BoundaryRectangle m_boundaryRect = new BoundaryRectangle();
-	
-	protected Group m_group;
-	private Matrix m_transform;
 	
 	public FilledRect() 
 	{
@@ -39,20 +35,10 @@ public class FilledRect implements GraphicalObject {
 	
 	// Getters and setters
 	
-	// TODO:public FilledRect (int x, int y, int width, int height,  Color color);
-	// TODO:public Line (int x1, int y1, int x2, int y2, Color color, int lineThickness);
-	// TODO: public Icon (Bitmap image, int x, int y);
-	// TOOD: public Text (String text, int x, int y, Typeface font, int fontSize; Coor color);
-
 	@Override
 	public void draw(Canvas graphics, Path clipShape) {
 		graphics.clipPath(clipShape);
 		graphics.drawRect(m_rect, m_paint);
-	}
-
-	@Override
-	public BoundaryRectangle getBoundingBox() {
-		return m_boundaryRect;
 	}
 
 	@Override
@@ -62,33 +48,6 @@ public class FilledRect implements GraphicalObject {
 		setY(y);
 	}
 
-	@Override
-	public Group getGroup() {
-		return m_group;
-	}
-
-	@Override
-	public void setGroup(Group group) {
-		m_group = group;
-	}
-
-	@Override
-	public boolean contains(int x, int y) {
-		int relx = x - m_rect.left;
-		int rely = y - m_rect.top;
-		return relx >= 0 && rely >= 0 && relx < m_rect.width() && rely < m_rect.height();
-	}
-
-	@Override
-	public void setAffineTransform(Matrix af) {
-		m_transform = af;
-	}
-
-	@Override
-	public Matrix getAffineTransform() {
-		return m_transform;
-	}
-
 	protected void updateAndDamage()
 	{
 		m_boundaryRect.x =  m_rect.left - (int)(m_paint.getStrokeWidth() / 2);
@@ -96,8 +55,7 @@ public class FilledRect implements GraphicalObject {
 		m_boundaryRect.width = m_rect.width() + (int)m_paint.getStrokeWidth();
 		m_boundaryRect.height = m_rect.height() + (int)m_paint.getStrokeWidth();
 		
-		if(m_group != null)
-			m_group.damage(m_boundaryRect);
+		doDamage();
 	}
 	
 	// Getters and setters
---
+++ Homework2Android/src/homework2/android/GraphicalObjectBase.java
@@ -0,0 +1,66 @@
+package homework2.android;
+
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Path;
+
+public class GraphicalObjectBase implements GraphicalObject {
+
+	protected Group m_group;
+	protected Matrix m_transform;
+	protected BoundaryRectangle m_boundaryRect = new BoundaryRectangle();
+	
+	public GraphicalObjectBase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doDamage()
+	{
+		if(m_group != null)
+			m_group.damage(m_boundaryRect);		
+	}
+	
+	@Override
+	public void draw(Canvas graphics, Path clipShape) {
+		// nop
+	}
+
+	@Override
+	public BoundaryRectangle getBoundingBox() {
+		return m_boundaryRect;
+	}
+
+	@Override
+	public void moveTo(int x, int y) {
+		// nop
+	}
+
+	@Override
+	public Group getGroup() {
+		return m_group;
+	}
+
+	@Override
+	public void setGroup(Group group) {
+		m_group = group;
+	}
+
+	@Override
+	public boolean contains(int x, int y) {
+		int relx = x - m_boundaryRect.x;
+		int rely = y - m_boundaryRect.y;
+		return relx >= 0 && rely >= 0 && relx < m_boundaryRect.width && rely < m_boundaryRect.height;
+	}
+
+	@Override
+	public void setAffineTransform(Matrix af) {
+		m_transform = af;
+
+	}
+
+	@Override
+	public Matrix getAffineTransform() {
+		return m_transform;
+	}
+
+}
