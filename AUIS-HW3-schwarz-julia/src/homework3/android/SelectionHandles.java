10314d816640e696c4fedaca1522afb3f1e43285
julenka <julenka@cs.cmu.edu>
2013-03-09 23:05:43 -0500

    proper selection handles

--- AUIS-HW3-schwarz-julia/src/homework3/android/SelectionHandles.java
+++ AUIS-HW3-schwarz-julia/src/homework3/android/SelectionHandles.java
@@ -9,19 +9,21 @@ import android.graphics.Path.Direction;
 import android.graphics.Path;
 
 public class SelectionHandles extends SimpleGroup implements Selectable {
-	static final int HANDLE_SIZE = 2;
+	static final int HANDLE_SIZE = 10;
 	private boolean m_interimSelected = false;
 	private boolean m_selected = false;
 
-	private static Paint m_selectionPaint;
-
-	static
+	private Paint m_selectionPaint;
 	{
 		m_selectionPaint = new Paint();
-		m_selectionPaint.setStyle(Style.STROKE);
+		m_selectionPaint.setStyle(Style.FILL);
 		m_selectionPaint.setColor(Color.GRAY);
-		m_selectionPaint.setStrokeWidth(HANDLE_SIZE);
 	}
+	
+	RectF m_drawRect = new RectF();
+
+
+
 	public SelectionHandles(int x, int y, int width, int height) {
 		super(x, y, width, height);
 	}
@@ -45,21 +47,51 @@ public class SelectionHandles extends SimpleGroup implements Selectable {
 		m_clipPath.addRect(new RectF(0, 0, m_width, m_height), Direction.CCW);
 	}
 
+	private void setCornerRect(boolean bottom, boolean right)
+	{
+		int left = m_x;
+		int top = m_y;
+		if(bottom)
+		{
+			top = m_y + m_boundaryRect.height - HANDLE_SIZE;
+		} 
+		
+		if(right)
+		{
+			left = m_x + m_boundaryRect.width - HANDLE_SIZE;
+		} 	
+		
+		m_drawRect.left = left;
+		m_drawRect.top = top;
+		m_drawRect.bottom = top + HANDLE_SIZE;
+		m_drawRect.right = left + HANDLE_SIZE;
+	}
+	
 	@Override
 	public void doDraw(Canvas graphics, Path clipShape) {
 		super.doDraw(graphics, clipShape);
+		
 		if(m_interimSelected)
 		{
 			// TODO: draw the entire rect inside the region, offsetting corectly
 			m_selectionPaint.setColor(Color.GRAY);
-			graphics.drawRect(boundaryRectangleToRect(m_boundaryRect), m_selectionPaint);	
+			drawCorners(graphics);
 		}
 		else if(m_selected)
 		{
 			m_selectionPaint.setColor(Color.BLACK);
-			graphics.drawRect(boundaryRectangleToRect(m_boundaryRect), m_selectionPaint);
+			drawCorners(graphics);
 		}
-
+		
+	}
+	
+	private void drawCorners(Canvas graphics)
+	{
+		for(int i = 0; i < 4; i++)
+		{
+				setCornerRect(i / 2 == 0, i % 2 == 0);
+				graphics.drawRect(m_drawRect, m_selectionPaint);	
+		}
 	}
 
 	@Override
