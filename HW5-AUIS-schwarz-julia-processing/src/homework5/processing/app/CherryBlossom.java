199744b33a80f7966be3d06e55814db567a9a17b
julenka <julenka@cs.cmu.edu>
2013-05-05 17:47:45 -0400

    Sprucing up the blossoms demo a bit

--- HW5-AUIS-schwarz-julia-processing/src/homework5/processing/app/CherryBlossom.java
+++ HW5-AUIS-schwarz-julia-processing/src/homework5/processing/app/CherryBlossom.java
@@ -4,11 +4,13 @@ import java.awt.Color;
 
 import homework5.processing.behavior.MoveXBehavior;
 import homework5.processing.constraints.IPropertyChangedListener;
+import homework5.processing.core.BoundaryRectangle;
 import homework5.processing.core.InteractiveFrame;
 import homework5.processing.graphicalobject.LayoutGroup;
 import homework5.processing.graphicalobject.Slider;
 import homework5.processing.graphicalobject.Text;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 /*****************************************************************************
  * Cherry Blossom Generator - Create new cherry trees with a mouse click!      *
@@ -50,11 +52,18 @@ public class CherryBlossom extends PApplet {
 	float treeHeight = 100.0f;
 	float randomness = 0.0f;
 	
+	PImage treeImage;
+	
+	
+	boolean isSnowing = true;
+	boolean UIVisible = true;
+	
 	public void setup() {
 		size(700, 500, JAVA2D);
 		frame = new InteractiveFrame(this);
 		smooth();
 		noStroke();
+		setupSnow();
 		setupUI();
 	}
 	
@@ -120,10 +129,21 @@ public class CherryBlossom extends PApplet {
 		if(refreshTree)
 		{
 			drawTree();
+			g.endDraw();
+			treeImage = new PImage(g.image);
 			refreshTree = false;
+		} else {
+			if(treeImage != null)
+			{
+				image(treeImage, 0, 0);
+				frame.damage(new BoundaryRectangle(0,0, width, height));
+			}
 		}
+		g.beginDraw();
+		if(UIVisible) frame.draw();
+		if(isSnowing) drawSnow();
+		g.endDraw();
 		
-		frame.draw();
 	}
 
 	void drawTree()
@@ -234,6 +254,111 @@ public class CherryBlossom extends PApplet {
 		}
 	}
 
+	int numberBalls = 400;
+	int numberSnow = 800;
+	 
+	//this creates an empty array called myBalls that can contain numberBalls objects of the class Ball
+	Ball[] myBalls = new Ball[numberBalls];
+	Ball[] backsnow = new Ball[numberSnow];
+	 
+	void setupSnow(){	   
+		for(int i = 0; i<numberSnow; i++)
+		{
+			backsnow[i] = new Ball();
+			backsnow[i].myDiameter = 2;
+			backsnow[i].posX = random(0, width);
+			backsnow[i].posY = random(0, height);
+			backsnow[i].speedX = random(0, .5f);
+			backsnow[i].speedY = random(1, 2);
+			backsnow[i].r = random(60, 140);
+		}
+
+		for(int i =0; i<numberBalls; i++)
+		{
+			myBalls[i] = new Ball();
+			myBalls[i].myDiameter = 3;
+			myBalls[i].posX = random(0, width);
+			myBalls[i].posY = random(0, height);
+			myBalls[i].speedX = random(0, 1);
+			myBalls[i].speedY = random(1, 3);
+			myBalls[i].r = random(140, 255);
+			myBalls[i].a = 200;
+		}
+
+	}
+	 
+	void drawSnow()
+	{
+		for(int i =0; i<numberBalls; i++)
+		{
+			myBalls[i].update();
+			backsnow[i].update();
+		}
+	}
+	
+	public void keyPressed() {
+		switch(key) {
+		case 's' : isSnowing = !isSnowing; break;
+		case 'u' : case ' ' : UIVisible = !UIVisible; break;
+		default: return;
+		}
+	}
+	 
+	class Ball //this does not exist until you call it
+	{
+	  //these are properties of the class
+	  int myDiameter = 10;
+	  float r = 255;
+	  float g = 255;
+	  float b = 255;
+	  float a = 110;
+	  float posX = 250; //these are the default properties of the class
+	  float posY = 250;
+	  float speedX = 3; //three pixels at a time
+	  float speedY = 2;
+	  //
+	   
+	  //this is the method
+	  //and it is a function inside the class, it can be whatever name you decide
+	  void update()
+	  {
+	   fill(r, a);
+	   ellipse(posX, posY, myDiameter, myDiameter);
+	  posX+= speedX;
+	  posY+= speedY;
+	   
+	  if(posX > width) //when you have only one instruction in your block of instruction, you don't have to use the curly brackets
+	  posX = 0;
+	   
+	  if(posX < 0)
+	  posX= width;
+	   
+	  if(posY > height)
+	  posY = 0;
+	   
+	  if(posY < 0)
+	  posY = height;
+	   
+	  if(speedY < 2)
+	  {
+	  myDiameter = 3;
+	  r = 130;
+	  }
+	   
+	  if(speedY < 1.6)
+	  {
+	  myDiameter = 2;
+	  r = 100;
+	  }
+	   
+	  if(speedY < 1.2)
+	  {
+	  myDiameter = 1;
+	  r = 70;
+	  }
+	   
+	  }
+	}
 
 
 }
